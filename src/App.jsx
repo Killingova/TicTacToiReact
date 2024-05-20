2c4e1a217ad2453a4e2737a55c4f2e5624092bda
Kristin Zhivkova <k.zhivkova@bbqberlin.onmicrosoft.com>
2024-05-20 22:15:11 +0200

    Deine Commit Nachricht hier

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react';
-import Log from './components/Log';
-// Import der GameBoard- und Player-Komponenten
-import GameBoard from "./components/GameBoard";
+
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
+import GameOver from "./components/GameOver";
+import {WINNING_COMBINATIONS} from "./winning-combinations";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
@@ -12,48 +20,77 @@ function deriveActivePlayer(gameTurns) {
   }
 
   return currentPlayer;
-
 }
 
-// Hauptkomponente der App
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  // useState Hook zur Verwaltung des activePlayer-Status, initialisiert auf 'X'
-      // const [activePlayer, setActivePlayer] = useState('X');
 
-const activePlayer = deriveActivePlayer(gameTurns);
+  const activePlayer = deriveActivePlayer(gameTurns);
 
-  // Funktion zur Behandlung der Auswahl eines Spielfeldes und zum Umschalten des aktiven Spielers
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+        
+        gameBoard[row][col] = player;
+    }
+
+    let winner;
+
+
+  for (const combination of WINNING_COMBINATIONS) {
+
+    const firstSquareSymbol = 
+    gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = 
+    gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = 
+    gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol && 
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+ 
+
   function handleSelectSquare(rowIndex, colIndex) {
-    // Aktualisieren des activePlayer-Status, Umschalten zwischen 'X' und 'O'
-        // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-     setGameTurns((prevTurns) => {
+
+    setGameTurns((prevTurns) => {
       const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
-        {square: {row: rowIndex, col: colIndex}, player: currentPlayer} , ...prevTurns
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer }, 
+        ...prevTurns,
       ];
 
       return updatedTurns;
     });
   }
 
-  // Rückgabe der Hauptstruktur des Spiels
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          {/* Rendern der Player-Komponenten, Weitergabe von initialName, symbol und isActive Props */}
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player 
+            initialName="Player 1" 
+            symbol="X" 
+            isActive={activePlayer === 'X'} />
+          <Player 
+            initialName="Player 2" 
+            symbol="O" 
+            isActive={activePlayer === 'O'} />
         </ol>
-        {/* Rendern der GameBoard-Komponente, Weitergabe der handleSelectSquare- und activePlayerSymbol-Props */}
-        <GameBoard
-        onSelectSquare={handleSelectSquare}
-        turns = {gameTurns} 
-        />
+        {(winner || hasDraw) && <GameOver winner={winner}/> }
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,54 +1,22 @@
-
-
-// Initiales Spielfeld, ein 3x3-Array mit null-Werten
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-// Definition der GameBoard-Komponente, die onSelectSquare und activePlayerSymbol als Props entgegennimmt
-export default function GameBoard({ onSelectSquare, turns}) {
-let gameBoard = initialGameBoard;
+export default function GameBoard({ onSelectSquare, board }) {
 
-for(const turn of turns) {
-    const {square, player} = turn;
-    const {row,col} = square;
 
-    gameBoard[row][col] = player;
-}
-
-/*     // useState Hook zur Verwaltung des gameBoard-Status, initialisiert mit initialGameBoard
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // Funktion zur Behandlung der Auswahl eines Spielfeldes
-    function handleSelectSquare(rowIndex, colIndex) {
-        // Aktualisierung des gameBoard-Status
-        setGameBoard((prevGameBoard) => {
-            // Erstellen einer Kopie des Spielfelds
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            // Setzen des Symbols des aktiven Spielers in das ausgewählte Feld
-            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-
-        // Aufrufen der onSelectSquare-Funktion, um den aktiven Spieler zu wechseln
-        onSelectSquare();
-    } */
-
-    // Rückgabe der Struktur des Spielfelds
     return (
         <ol id="game-board">
-            {/* Iterieren über die Zeilen des Spielfelds */}
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
-                        {/* Iterieren über die Spalten der Zeilen */}
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                {/* Button zur Auswahl eines Spielfeldes, der handleSelectSquare aufruft */}
-                                <button onClick={ () => onSelectSquare(rowIndex,colIndex)} 
-                                disabled={playerSymbol !== null}
+                                <button 
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)} 
+                                    disabled={playerSymbol !== null}
                                 >
                                     {playerSymbol}
                                 </button>
